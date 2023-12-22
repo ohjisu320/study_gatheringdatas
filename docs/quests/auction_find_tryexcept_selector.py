fa90b6807480ef195c0e3865adb3ab23afc5b507
ohjisu320 <ohjisu320@gmail.com>
2023-12-22 15:58:14 +0900

    commit

---
+++ docs/quests/auction_find_tryexcept_selector.py
@@ -0,0 +1,45 @@
+#  상품 제목, 판매원가, 할인가, 배송방법(list방식) 크롤링하기
+# 각각 사용했던 태그 공유
+# git url / file: 사용한 각각 tag(element) ex) div title
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+webdriver_manager_directory = ChromeDriverManager().install()
+
+browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
+
+capabilities = browser.capabilities
+
+browser.get("https://corners.auction.co.kr/corner/categorybest.aspx")
+
+# - 가능 여부에 대한 OK 받음
+pass
+
+# 정보 획득
+from selenium.webdriver.common.by import By
+selector_value = "div.info"
+element_bundle = browser.find_elements(by=By.CSS_SELECTOR, value=selector_value)
+for element_item in element_bundle :
+    element_title = element_item.find_element(by=By.CSS_SELECTOR, value="em > a")
+    title = element_title.text
+    try : 
+        element_old_price = element_item.find_element(by=By.CSS_SELECTOR, value="ul > li.c_price > span > strike > span")
+        old_price = element_old_price.text
+        element_new_price = element_item.find_element(by=By.CSS_SELECTOR, value="ul > li.d_price > span.sale > span")
+        new_price = element_new_price.text
+        element_delivery = element_item.find_element(by=By.CSS_SELECTOR, value="div.icon > div")
+        delivery = element_delivery.text        
+        pass
+    except :
+        old_price = ""
+        element_new_price = element_item.find_element(by=By.CSS_SELECTOR, value="ul > li.d_price > span.sale > span")
+        new_price = element_new_price.text
+        element_delivery = element_item.find_element(by=By.CSS_SELECTOR, value="div.icon > div")
+        delivery = element_delivery.text
+        pass
+    finally :
+        pass
+    print("title : {}, old price : {}, new price : {}, delivery : {}".format(title, old_price, new_price, delivery))
+    pass
